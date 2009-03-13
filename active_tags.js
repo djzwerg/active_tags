584e404f198c61aa4ce0cc2457189aabb5ebaf44
dragonwize <dragonwize@137882.no-reply.drupal.org>
2009-03-13 01:55:28 +0000

    Minor clean-up

--- active_tags.js
+++ active_tags.js
@@ -18,7 +18,8 @@ function activetags_activate(context) {
 
   if ($.browser.mozilla) {
     $('.tag-entry:not(.tag-processed)').keypress(activetags_check_enter).addClass('tag-processed');
-  } else {
+  }
+  else {
     $('.tag-entry:not(.tag-processed)').keydown(activetags_check_enter).addClass('tag-processed');
   }
 
@@ -44,8 +45,8 @@ function activetags_check_enter(event) {
 
 function activetags_add(context, v) {
   if (jQuery.trim(v) != '') {
-    $(context).prev().children('.tag-holder').append('<div class="tag-tag"><span class="tag-text">'+
-      jQuery.trim(v)+'</span><span class="remove-tag">x</span></div>');
+    $(context).prev().children('.tag-holder').append('<div class="tag-tag"><span class="tag-text">' +
+      jQuery.trim(v) + '</span><span class="remove-tag">x</span></div>');
     $('.remove-tag:not(.tag-processed)').click(function() {
       $(this).parent().remove();
       activetags_update(context);
@@ -58,7 +59,8 @@ function activetags_update(context) {
   $(context).prev().children('.tag-holder').children().children('.tag-text').each(function(i) {
     if(i==0) {
       $(context).children('input.form-text').val($(this).text());
-    } else {
+    }
+    else {
       $(context).children('input.form-text').val(
         $(context).children('input.form-text').val() + ', ' + $(this).text()
       );
@@ -68,13 +70,13 @@ function activetags_update(context) {
 
 function activetags_widget(context) {
   var vid = context.substr(20,1);
-  return '<div id="'+context+'-activetags" class="form-item">'+
-    '<label for="'+context+'-edit-tags">'+ $(context + ' label').text() +'</label>'+
-    '<div class="tag-holder"></div>'+
-    '<input type="text" class="tag-entry form-autocomplete" size="30" id="active-tag-edit0'+vid+'" />'+
-    '<input type="button" value="add" class="add-tag">'+
-    '<input class="autocomplete" type="hidden" id="active-tag-edit0'+vid+'-autocomplete" '+
-    'value="'+Drupal.settings.basePath+'taxonomy/autocomplete/'+vid+'" disabled="disabled" />'+
+  return '<div id="' + context + '-activetags" class="form-item">' +
+    '<label for="' + context + '-edit-tags">' + $(context + ' label').text() + '</label>' +
+    '<div class="tag-holder"></div>' +
+    '<input type="text" class="tag-entry form-autocomplete" size="30" id="active-tag-edit0'+vid+'" />' +
+    '<input type="button" value="add" class="add-tag">' +
+    '<input class="autocomplete" type="hidden" id="active-tag-edit0' + vid + '-autocomplete" ' +
+    'value="' + Drupal.settings.basePath + 'taxonomy/autocomplete/' + vid + '" disabled="disabled" />' +
   '</div>';
 }
 
